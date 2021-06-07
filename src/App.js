036a36ca7a7b2d71c3d37d47d5a968da2bf49aef
Matias Menillo <matiasmenillo@gmail.com>
2021-06-07 19:21:51 -0300

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <h1 className="title is-1"><i className="fas fa-home"></i> ¡Hola!</h1>
+  )
+}
+
+export default App;
